fb4043626e266c1f6ce640e156bc3533643390c2
tbrzyzek <tbrzyzek@gmail.com>
2017-05-10 11:36:14 -0400

    Delete hashfxn.m

--- hashfxn.m
+++
@@ -1,7 +0,0 @@
-% hashfxn() calculates a location by taking in three parameters, the peak
-% frequency, DT (IDK WHAT THIS IS), and SIZE(IDK WHAT THIS IS PLS DEFINE).
-function hashme = hashfxn(pk, dt, size)
-    % define this thing below
-    hash = mod(round(), size)+1;
-
-end
